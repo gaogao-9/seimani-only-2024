e26c51d024dc797f7c85ed417e24412e27ebde31
gaogao-9 <gaotuki@gmail.com>
2024-08-14 22:02:00 +0900

    feat: アフターイベント情報を追加

--- src/app/event/page.client.tsx
+++ src/app/event/page.client.tsx
@@ -71,10 +71,27 @@ const Page: React.FC = () => {
             <br />
             来場の記念に総理の皆さんからヒトコト～クダサイ～オネガイ～
           </StyledCardSection>
-          <StyledCardSection delay={delayCount++ / 10} title="アフターイベント">
-            即売会パートの後に開催予定！
+        </Card>
+        <Card title={<Card.Title>アフターイベント</Card.Title>}>
+          <StyledCardSection
+            delay={delayCount++ / 10}
+            title="オンリーイベントでオンリー総理を探せ！"
+          >
+            毎年多種多様な総理たちが集うソクバイ海。多くの総理がいるオンリー会場の中で、一人しかいない個性の総理を探しましょう！
             <br />
-            企画詳細は後日お知らせします。
+            予め会場内の総理たちに総理の特徴を一つ書いてもらい、その中から書いて貰った特徴を持つ総理がいらっしゃるか聞いていきます。
+            <br />
+            特徴を持つ総理がオンリー会場に一人だけだった場合、一人だけの総理を当てた側と当てられた側の二名には景品をプレゼント！
+          </StyledCardSection>
+          <StyledCardSection
+            delay={delayCount++ / 10}
+            title="再復刻&nbsp;政マニアック！クイズ大会！"
+          >
+            二度あることは三度ある！&nbsp;智で智を洗え！&nbsp;政マニに関するクイズ大会！
+            <br />
+            アフター全員参加の○×クイズ大会です。こちらも最後まで残った一人には豪華景品をプレゼント。
+            <br />
+            政マニに関する復習をして、目指せ！クイズ王総理！
           </StyledCardSection>
         </Card>
       </Flex>
